999274e0b444d252ce0ec565536fe5198a642ba9
LeeHsss <lii0730@icloud.com>
2022-02-07 22:32:20 +0900

    Initial Commit

---
+++ MoneyManagement/MoneyManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyManagementApp.swift
+//  MoneyManagement
+//
+//  Created by LeeHsss on 2022/02/07.
+//
+
+import SwiftUI
+
+@main
+struct MoneyManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
